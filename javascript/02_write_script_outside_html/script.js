43bf47d9a6857f961973229a2de4cd43e41599e7
pf9-arpit <arpit@platform9.com>
2025-04-05 18:16:55 +0530

    buttom click function

---
+++ javascript/02_write_script_outside_html/script.js
@@ -0,0 +1,11 @@
+function doClick(){
+    document.getElementById("pg1").innerHTML = "this is paragraph version 2 outside html";
+    
+    /*
+        similarily changing css for h1 tag
+        document.getElementsByTagName("h1")[0].style.color = 'red';
+    */
+   
+}
+
+
